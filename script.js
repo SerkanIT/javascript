eead59660318c254398d63308052eda47c2d34a1
SerkanIT <157528566+SerkanIT@users.noreply.github.com>
2024-02-06 18:39:04 +0500

    Java Script
    Prikol Bilan

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+alert("Are you sure?")
+confirm("Do you want see this?")
+confirm("Its maybe be a virus)")
+confirm("I ask last time")
+alert("OKAY!")
+
+let  a = prompt("What is your Name?")
+console.log("Name: ",a);
+let b = prompt("What is your Last Name?")
+console.log("Last Name: ", b);
+let c = prompt("How old are you?")
+console.log("Age: ",c);
+alert("HAHA")
+alert("Its a joke!!")
